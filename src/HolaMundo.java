d147c6a13f381da39dc1af0cbe357bef6f0fe4f3
MattCostaRusso <mattcostarusso@gmail.com>
2025-04-14 02:30:09 -0300

    Este es mi primer commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo");
+        int a = 1;
+        int b = 2;
+
+        System.out.println("Resultado de a + b es : " + (a + b));
+    }
+}
